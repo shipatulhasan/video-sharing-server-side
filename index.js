82d65665ceee58f348883068c7ecb6fe09402869
shipatulhasan <shipatulhasan328@gmail.com>
2023-01-16 18:14:35 +0600

    comment correction

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
@@ -25,11 +25,41 @@ const run = async()=>{
     try{
         const usersCollection = client.db("videoSharing").collection("users")
         const productsCollection = client.db("videoSharing").collection("products")
+        const commentsCollection = client.db("videoSharing").collection("comments")
+
+        // get user role
+
+
+        app.get('/user/:email',async(req,res)=>{
+            const email = req.params.email
+            const filter = {email:email}
+            const user = await usersCollection.findOne(filter)
+            if(user.role==='admin'){
+                return res.send({isAdmin:true})
+            }
+            res.send({isAdmin:false})
+        })
 
         // get product from database
 
         app.get('/product',async(req,res)=>{
             const result = await productsCollection.find({}).toArray()
+            res.send(result)
+        })
+       
+        app.get('/product/:id',async(req,res)=>{
+            const id = req.params.id
+            const filter = {_id:ObjectId(id)}
+            const result = await productsCollection.findOne(filter)
+            res.send(result)
+        })
+
+        // get comments
+
+        app.get('/comments/:id',async(req,res)=>{
+            const id = req.params.id
+            const filter = {productId:id}
+            const result = await commentsCollection.find(filter).toArray()
             res.send(result)
         })
 
@@ -43,6 +73,11 @@ const run = async()=>{
         app.post('/product',async(req,res)=>{
             const product = req.body
             const result = await productsCollection.insertOne(product)
+            res.send(result)
+        })
+        app.post('/comment',async(req,res)=>{
+            const comment = req.body
+            const result = await commentsCollection.insertOne(comment)
             res.send(result)
         })
     }
